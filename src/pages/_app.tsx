7dd8b8f8da015c9bf0f137eb1fdb92526212e1a3
kapustyansky <kapustianskyov@gmail.com>
2026-05-07 18:49:21 +0000

    Add Inter variable font and update global styles
    - Introduced Inter-Variable.woff2 font for improved typography.
    - Enhanced global styles with new CSS variables and layout adjustments.
    - Refactored _app.tsx to support layout rendering.
    - Updated _document.tsx for consistent component structure.
    - Created variables.css for future CSS variable definitions.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,36 @@
 import '@/styles/globals.css';
+import '@/styles/variables.css';
 
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+import { NextPage } from 'next/types';
+import type { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+	getLayout?: (page: ReactElement) => ReactNode;
+};
+
+const Inter = localFont({
+	src: '../../public/assets/fonts/Inter-Variable.woff2',
+	display: 'swap',
+});
+
+const App = ({
+	Component,
+	pageProps,
+}: AppProps & { Component: NextPageWithLayout }) => {
+	const getLayout = Component.getLayout ?? ((page) => page);
+
+	return (
+		<>
+			<style jsx global>{`
+				html {
+					--font-inter: ${Inter.style.fontFamily};
+				}
+			`}</style>
+			{getLayout(<Component {...pageProps} />)}
+		</>
+	);
+};
+
+export default App;
